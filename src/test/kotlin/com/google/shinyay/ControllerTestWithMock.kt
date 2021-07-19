e57061e8989fec3127a1d32ed6911a666e22ec57
shinyay <shinya.com@gmail.com>
2021-07-19 09:53:42 +0900

    Add an integration test to post book data as json to database through web layer

--- src/test/kotlin/com/google/shinyay/ControllerTestWithMock.kt
+++ src/test/kotlin/com/google/shinyay/ControllerTestWithMock.kt
@@ -1,12 +1,16 @@
 package com.google.shinyay
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.google.shinyay.entity.Book
 import org.hamcrest.Matchers.containsString
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
@@ -30,4 +34,14 @@ class ControllerTestWithMock(@Autowired val mockMvc: MockMvc) {
             .andExpect(status().isOk)
     }
 
+    @Test
+    fun bookRegistrationThroughAllLayers() {
+        val testBook = Book(title = "Testing", author = "shinyay", price = 1000)
+
+        mockMvc.perform(post("/book")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(ObjectMapper().writeValueAsString(testBook))
+        )
+            .andExpect(status().isOk)
+    }
 }
