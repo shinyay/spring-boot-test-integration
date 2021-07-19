4be430f24392065811b76f15afd9f057c97bae18
shinyay <shinya.com@gmail.com>
2021-07-19 14:11:24 +0900

    Add a funcuton for the book registration

--- src/main/kotlin/com/google/shinyay/controller/BookController.kt
+++ src/main/kotlin/com/google/shinyay/controller/BookController.kt
@@ -2,6 +2,7 @@ package com.google.shinyay.controller
 
 import com.google.shinyay.entity.Book
 import com.google.shinyay.service.BookService
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -9,8 +10,13 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class BookController(val service: BookService) {
 
+//    @PostMapping("/book")
+//    fun displayBook(@RequestBody book:Book): Book {
+//        return service.displayBook(book)
+//    }
+
     @PostMapping("/book")
-    fun displayBook(@RequestBody book:Book): Book {
-        return service.displayBook(book)
+    fun registerBook(@RequestBody book:Book): Book {
+        return service.registerBook(book)
     }
 }
