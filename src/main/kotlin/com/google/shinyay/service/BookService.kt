95358c3d4851849ecd15bff2adcbdc8debf94a34
shinyay <shinya.com@gmail.com>
2021-07-19 12:22:05 +0900

    Add persistence function

--- src/main/kotlin/com/google/shinyay/service/BookService.kt
+++ src/main/kotlin/com/google/shinyay/service/BookService.kt
@@ -1,10 +1,15 @@
 package com.google.shinyay.service
 
 import com.google.shinyay.entity.Book
+import com.google.shinyay.repository.BookDaoRepository
 import org.springframework.stereotype.Service
 
 @Service
-class BookService {
+class BookService(val repository: BookDaoRepository) {
 
     fun displayBook(book: Book) = book
+
+    fun registerBook(book: Book): Book {
+        return repository.save(book)
+    }
 }
